e25c75a2258f8c395ca91a16c4977c7b6679a525
Japem8 <penavades@gmail.com>
2023-01-16 18:11:41 +0100

    Comienzo de la resolucion del ejercicio01

---
+++ src/main/java/com/unisys/ejercicios/Ejercicio01.java
@@ -0,0 +1,20 @@
+package com.unisys.ejercicios;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        int numero=3;
+        short marianico=5;
+        long gasol=755;
+        boolean independencia=false;
+        double presupuesto=45000;
+        float longitud= 50.5f;
+        var suma=gasol+marianico;
+        System.out.println(numero);
+        System.out.println(marianico);
+        System.out.println(gasol);
+        System.out.println(independencia);
+        System.out.println(presupuesto);
+        System.out.println(longitud);
+        System.out.println(suma);
+    }
+}
