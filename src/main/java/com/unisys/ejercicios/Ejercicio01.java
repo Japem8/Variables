0869670608e9c6dc4bf08df15305bcfa5084b60b
Japem8 <penavades@gmail.com>
2023-01-16 18:47:29 +0100

    Realización del ejercicio01

--- src/main/java/com/unisys/ejercicios/Ejercicio01.java
+++ src/main/java/com/unisys/ejercicios/Ejercicio01.java
@@ -1,5 +1,11 @@
 package com.unisys.ejercicios;
-
+/** Esto es un comentario para probar el javadoc
+ * este texto aparecerá en el archivo HTML generado.
+ * <strong>Realizado en enero 2023</strong>
+ *
+ * @author Javier Penavades
+ * @version 1.0
+ */
 public class Ejercicio01 {
     public static void main(String[] args) {
         int numero=3;
@@ -9,12 +15,12 @@ public class Ejercicio01 {
         double presupuesto=45000;
         float longitud= 50.5f;
         var suma=gasol+marianico;
-        System.out.println(numero);
-        System.out.println(marianico);
-        System.out.println(gasol);
-        System.out.println(independencia);
-        System.out.println(presupuesto);
-        System.out.println(longitud);
-        System.out.println(suma);
+        System.out.println("Contenido de numero "+numero);
+        System.out.println("Contenido de marianico "+marianico);
+        System.out.println("Contenido de gasol "+gasol);
+        System.out.println("Contenido de independencia "+independencia);
+        System.out.println("Contenido de presupuesto "+presupuesto);
+        System.out.println("Contenido de longitud "+longitud);
+        System.out.println("Contenido de suma "+suma);
     }
 }
