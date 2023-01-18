3f31f8799e598a53a92b66fddd4d2f733e5b2863
Japem8 <penavades@gmail.com>
2023-01-18 16:11:16 +0100

    Realización de Ejercicio04

---
+++ src/main/java/com/unisys/ejercicios/Ejercicio04.java
@@ -0,0 +1,19 @@
+package com.unisys.ejercicios;
+
+public class Ejercicio04 {
+    public static void main(String[] args) {
+        String saludo="Hola";
+        String saludo2="Adios";
+        System.out.println("Longitud de saludo: "+saludo.length());
+        char c4=saludo.charAt(3);
+        System.out.println("El caracter 4 de saludo es: "+c4);
+        System.out.println(saludo.toUpperCase());
+        System.out.println(saludo.toLowerCase());
+        System.out.println(saludo.indexOf("I"));
+        if (saludo.equals(saludo2)) {
+            System.out.println("Las cadenas son iguales");
+        }else {
+            System.out.println("Las cadenas son distintas");
+        }
+    }
+}
