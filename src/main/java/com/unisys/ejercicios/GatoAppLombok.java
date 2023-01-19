53e9ba3e0fa506e3277f074ccf14814b0549ff12
Japem8 <penavades@gmail.com>
2023-01-19 16:02:27 +0100

    Realización de ejercicio con lombok

---
+++ src/main/java/com/unisys/ejercicios/GatoAppLombok.java
@@ -0,0 +1,8 @@
+package com.unisys.ejercicios;
+
+public class GatoAppLombok {
+    public static void main(String[] args) {
+        GatoLombok Misifu=new GatoLombok("Misifu","12");
+        System.out.println(Misifu);
+    }
+}
---
+++ src/main/java/com/unisys/ejercicios/GatoLombok.java
@@ -0,0 +1,13 @@
+package com.unisys.ejercicios;
+
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GatoLombok {
+    private String name;
+    private String age;
+
+}
