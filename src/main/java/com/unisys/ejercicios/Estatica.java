bcb8ecd11915eae91eb3fe317d75a3b0d97ed317
Japem8 <penavades@gmail.com>
2023-01-19 16:47:48 +0100

    Mejora ejercicio de clase estatica

--- src/main/java/com/unisys/ejercicios/Estatica.java
+++ src/main/java/com/unisys/ejercicios/Estatica.java
@@ -2,7 +2,7 @@ package com.unisys.ejercicios;
 
 public class Estatica {
     public static float pi=3.1416f;
-    public static void berrinche() {
-        System.out.println("Aggggg");
+    public static String berrinche() {
+        return "Aggggg";
     }
 }
--- src/main/java/com/unisys/ejercicios/EstaticaApp.java
+++ src/main/java/com/unisys/ejercicios/EstaticaApp.java
@@ -3,7 +3,9 @@ package com.unisys.ejercicios;
 import static com.unisys.ejercicios.Estatica.berrinche;
 public class EstaticaApp {
     public static void main(String[] args) {
+        String queja;
         System.out.println(Estatica.pi);
-        berrinche();
+        queja=berrinche();
+        System.out.println(queja);
     }
 }
